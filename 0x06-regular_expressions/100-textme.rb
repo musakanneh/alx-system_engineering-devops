a19763967017a752fe63c22f96c776a86cda255f
musakanneh <mkanneh17@alustudent.com>
2021-02-22 11:44:39 +0200

    Exe mode activated

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,1 +1,5 @@
 #!/usr/bin/env ruby
+# Textme
+puts ARGV[0]
+       .scan(/(?<=from:|to:|flags:).*?(?=\])/)
+       .join(',')
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,1 +1,3 @@
 #!/usr/bin/env ruby
+# Not quite HBTN yet
+puts ARGV[0].scan(/^h.n$/).join

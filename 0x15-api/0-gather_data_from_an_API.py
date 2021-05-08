d971a3eaa8ad204cb3aa7ea72b6113e7fde4bfe1
musakanneh <mkanneh17@alustudent.com>
2021-05-08 11:15:58 +0200

    API Task ++

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""Gathering data from API placeholder"""
+"""Getting from API placeholder"""
 
 import requests
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    req_endpoint = 'https://jsonplaceholder.typicode.com'
+    userId = argv[1]
+    user = requests.get(
+        req_endpoint + 'users/{}'.format(userId), verify=False).json()
 
-if __name__ == "__main__":
-    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(sys.argv[1]))
-    d = r.json()
-    name = d.get('name')
-    user_id = d.get('id')
-    r = requests.get('https://jsonplaceholder.typicode.com/todos')
-    d_todos = r.json()
-    tasks, completed = 0, 0
-    titles = []
-    for i in d_todos:
-        if i.get('userId') == user_id:
-            tasks += 1
-            if i.get('completed'):
-                completed += 1
-                titles.append(i.get('title'))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, completed, tasks))
-    for i in titles:
-        print('\t', i)
+    todos = requests.get(
+        req_endpoint + 'todos?userId={}'.format(userId), verify=False).json()
+
+    completed_task = []
+    for task in todos:
+        if task.get('completed'):
+            completed_task.append(task.get('title'))
+
+    print("Employee {}is done with task({}/{}):"
+          .format(user.get('name'),
+                  len(completed_task), len(todos)))
+
+    for task in completed_task:
+        print('\t', task)

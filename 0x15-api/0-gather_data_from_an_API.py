43af3fe3428f9964136eb518aa788bdc451cca47
musakanneh <mkanneh17@alustudent.com>
2021-05-08 11:54:44 +0200

    Modified test one

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,21 +5,19 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    req_endpoint = "https://jsonplaceholder.typicode.com/"
+    """Gets API endpoint, then identify a user to display completed task info"""
+    endpoint = "https://jsonplaceholder.typicode.com/"
     userId = sys.argv[1]
-    user = requests.get(
-        req_endpoint + 'users/{}'.format(userId)).json()
-
-    todos = requests.get(
-        req_endpoint + 'todos?userId={}'.format(userId)).json()
+    user = requests.get(endpoint + 'users/{}'.format(userId)).json()
+    todo = requests.get(endpoint + 'todos?userId={}'.format(userId)).json()
 
     completed = []
-    for task in todos:
+    for task in todo:
         if task.get("completed"):
             completed.append(task.get("title"))
 
     print("Employee {} is done with task({}/{}):"
-          .format(user.get('name'), len(completed), len(todos)))
+          .format(user.get('name'), len(completed), len(todo)))
 
     for task in completed:
         print('\t', task)
